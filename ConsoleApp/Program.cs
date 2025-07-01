b17dfcbff357b40b312819674ad0fa1eba8bab19
Daniel-Liuz <yangyang031107@gmail.com>
2025-07-01 08:56:10 +0800

    first commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)//需要有一个入口，而main就是入口
+        {
+            Console.WriteLine("Hello,World!");//输出Hello, World!
+            Console.WriteLine("按任意键退出...");
+            Console.ReadKey();
+        }
+    }
+}
