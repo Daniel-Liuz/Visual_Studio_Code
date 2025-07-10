182d44076d733b7853c12ab542b11f554305bfb3
Daniel-Liuz <yangyang031107@gmail.com>
2025-07-11 01:50:06 +0800

    完成多验证码功能

--- WindowsFormsApp1/frmPasswordReset.Designer.cs
+++ WindowsFormsApp1/frmPasswordReset.Designer.cs
@@ -31,13 +31,22 @@
             this.label1 = new System.Windows.Forms.Label();
             this.txtResetStudentNo = new System.Windows.Forms.TextBox();
             this.btnSendResetEmail = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gbSecurityQuestion = new System.Windows.Forms.GroupBox();
+            this.txtAnswer1 = new System.Windows.Forms.TextBox();
+            this.lblQuestion2 = new System.Windows.Forms.Label();
+            this.txtAnswer2 = new System.Windows.Forms.TextBox();
+            this.btnVerifyAnswers = new System.Windows.Forms.Button();
+            this.lblQuestion1 = new System.Windows.Forms.Label();
+            this.gbSecurityQuestion.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("黑体", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(12, 85);
+            this.label1.Location = new System.Drawing.Point(6, 20);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(271, 33);
             this.label1.TabIndex = 0;
@@ -46,7 +55,7 @@
             // txtResetStudentNo
             // 
             this.txtResetStudentNo.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.txtResetStudentNo.Location = new System.Drawing.Point(289, 84);
+            this.txtResetStudentNo.Location = new System.Drawing.Point(324, 19);
             this.txtResetStudentNo.Name = "txtResetStudentNo";
             this.txtResetStudentNo.Size = new System.Drawing.Size(241, 42);
             this.txtResetStudentNo.TabIndex = 1;
@@ -54,7 +63,7 @@
             // btnSendResetEmail
             // 
             this.btnSendResetEmail.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.btnSendResetEmail.Location = new System.Drawing.Point(577, 85);
+            this.btnSendResetEmail.Location = new System.Drawing.Point(558, 101);
             this.btnSendResetEmail.Name = "btnSendResetEmail";
             this.btnSendResetEmail.Size = new System.Drawing.Size(195, 41);
             this.btnSendResetEmail.TabIndex = 2;
@@ -62,16 +71,102 @@
             this.btnSendResetEmail.UseVisualStyleBackColor = true;
             this.btnSendResetEmail.Click += new System.EventHandler(this.btnSendResetEmail_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("黑体", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(13, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(255, 33);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "方法1：邮箱找回";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("黑体", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(13, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(383, 33);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "方法2：使用安全问题找回";
+            // 
+            // gbSecurityQuestion
+            // 
+            this.gbSecurityQuestion.Controls.Add(this.lblQuestion1);
+            this.gbSecurityQuestion.Controls.Add(this.txtAnswer1);
+            this.gbSecurityQuestion.Controls.Add(this.btnVerifyAnswers);
+            this.gbSecurityQuestion.Controls.Add(this.lblQuestion2);
+            this.gbSecurityQuestion.Controls.Add(this.txtAnswer2);
+            this.gbSecurityQuestion.Location = new System.Drawing.Point(31, 241);
+            this.gbSecurityQuestion.Name = "gbSecurityQuestion";
+            this.gbSecurityQuestion.Size = new System.Drawing.Size(642, 411);
+            this.gbSecurityQuestion.TabIndex = 6;
+            this.gbSecurityQuestion.TabStop = false;
+            this.gbSecurityQuestion.Text = "安全问题验证";
+            // 
+            // txtAnswer1
+            // 
+            this.txtAnswer1.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtAnswer1.Location = new System.Drawing.Point(58, 129);
+            this.txtAnswer1.Name = "txtAnswer1";
+            this.txtAnswer1.Size = new System.Drawing.Size(288, 39);
+            this.txtAnswer1.TabIndex = 1;
+            // 
+            // lblQuestion2
+            // 
+            this.lblQuestion2.AutoSize = true;
+            this.lblQuestion2.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblQuestion2.Location = new System.Drawing.Point(53, 197);
+            this.lblQuestion2.Name = "lblQuestion2";
+            this.lblQuestion2.Size = new System.Drawing.Size(376, 28);
+            this.lblQuestion2.TabIndex = 2;
+            this.lblQuestion2.Text = "问题2：您的小学叫什么名字?";
+            // 
+            // txtAnswer2
+            // 
+            this.txtAnswer2.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtAnswer2.Location = new System.Drawing.Point(58, 243);
+            this.txtAnswer2.Name = "txtAnswer2";
+            this.txtAnswer2.Size = new System.Drawing.Size(288, 39);
+            this.txtAnswer2.TabIndex = 3;
+            // 
+            // btnVerifyAnswers
+            // 
+            this.btnVerifyAnswers.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnVerifyAnswers.Location = new System.Drawing.Point(58, 316);
+            this.btnVerifyAnswers.Name = "btnVerifyAnswers";
+            this.btnVerifyAnswers.Size = new System.Drawing.Size(288, 68);
+            this.btnVerifyAnswers.TabIndex = 4;
+            this.btnVerifyAnswers.Text = "验证答案";
+            this.btnVerifyAnswers.UseVisualStyleBackColor = true;
+            this.btnVerifyAnswers.Click += new System.EventHandler(this.btnVerifyAnswers_Click);
+            // 
+            // lblQuestion1
+            // 
+            this.lblQuestion1.AutoSize = true;
+            this.lblQuestion1.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblQuestion1.Location = new System.Drawing.Point(54, 81);
+            this.lblQuestion1.Name = "lblQuestion1";
+            this.lblQuestion1.Size = new System.Drawing.Size(362, 28);
+            this.lblQuestion1.TabIndex = 5;
+            this.lblQuestion1.Text = "问题1：您最喜欢什么颜色？";
+            // 
             // frmPasswordReset
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1090, 993);
+            this.Controls.Add(this.gbSecurityQuestion);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnSendResetEmail);
-            this.Controls.Add(this.txtResetStudentNo);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResetStudentNo);
             this.Name = "frmPasswordReset";
             this.Text = "frmPasswordReset";
+            this.gbSecurityQuestion.ResumeLayout(false);
+            this.gbSecurityQuestion.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,5 +177,13 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtResetStudentNo;
         private System.Windows.Forms.Button btnSendResetEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox gbSecurityQuestion;
+        private System.Windows.Forms.Label lblQuestion1;
+        private System.Windows.Forms.TextBox txtAnswer1;
+        private System.Windows.Forms.Button btnVerifyAnswers;
+        private System.Windows.Forms.Label lblQuestion2;
+        private System.Windows.Forms.TextBox txtAnswer2;
     }
 }
--- WindowsFormsApp1/frmPasswordReset.cs
+++ WindowsFormsApp1/frmPasswordReset.cs
@@ -1,89 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using SQL;
 using System.Net;
 using System.Net.Mail;
 using System.Security.Cryptography;
 using System.Text;
+using System.Windows.Forms;
+using SQL; // 请确保这是您的 SQLHelper 所在的命名空间
 
 namespace WindowsFormsApp1
 {
     public partial class frmPasswordReset : Form
     {
+        // 【硬编码】指定“安全问题”功能仅对这个特定学号生效
+        private const string TargetStudentNo = "10235501456";
+
         public frmPasswordReset()
         {
             InitializeComponent();
         }
-        private string HashPassword(string plainText)
+
+        // 窗体加载时，确保安全问题组合框是隐藏的
+        private void frmPasswordReset_Load(object sender, EventArgs e)
         {
-            if (string.IsNullOrEmpty(plainText))
-            {
-                return string.Empty;
-            }
-
-            using (SHA256 sha256Hash = SHA256.Create())
-            {
-                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(plainText));
-
-                StringBuilder builder = new StringBuilder();
-                for (int i = 0; i < bytes.Length; i++)
-                {
-                    builder.Append(bytes[i].ToString("x2"));
-                }
-                return builder.ToString();
-            }
+            gbSecurityQuestion.Visible = false;
         }
-        private void SendEmail(string recipientEmail, string password)
-        {
-            // --- 已更新为您的QQ邮箱信息 ---
-            string fromEmail = "1195773178@qq.com";        // 您的QQ邮箱地址
-            string fromPassword = "jidwtfljqebkicej";      // 您的QQ邮箱授权码
-            string smtpHost = "smtp.qq.com";               // QQ邮箱的SMTP服务器地址
-            int smtpPort = 587;                            // QQ邮箱推荐的端口号，启用STARTTLS加密
 
-            // 创建邮件发送客户端
-            SmtpClient client = new SmtpClient(smtpHost, smtpPort);
+        #region =========== 1. 通过邮箱找回 ===========
 
-            // 关键：对于587端口，必须启用SSL，这会自动使用STARTTLS安全连接
-            client.EnableSsl = true;
-
-            client.UseDefaultCredentials = false;
-            // 提供发件人的凭据（QQ邮箱地址和授权码）
-            client.Credentials = new System.Net.NetworkCredential(fromEmail, fromPassword);
-
-            // 创建邮件内容对象
-            MailMessage mailMessage = new MailMessage();
-            // 设置发件人，可以额外指定一个显示名称
-            mailMessage.From = new MailAddress(fromEmail, "学生管理系统");
-            // 添加收件人
-            mailMessage.To.Add(recipientEmail);
-            // 设置邮件主题
-            mailMessage.Subject = "【学生管理系统】 - 密码重置通知";
-            // 设置邮件正文
-            mailMessage.Body = string.Format(
-                "您好：\n\n" +
-                "您正在使用密码重置功能。您的新临时密码是：\n\n" +
-                "【 {0} 】\n\n" +
-                "请使用此密码登录系统，并尽快在个人中心修改为您自己的常用密码。\n\n" +
-                "【学生管理系统】",
-                password
-            );
-            // 确保邮件正文是纯文本格式
-            mailMessage.IsBodyHtml = false;
-
-            // 正式发送邮件
-            client.Send(mailMessage);
-        }
+        // “发送重置邮件”按钮的逻辑
         private void btnSendResetEmail_Click(object sender, EventArgs e)
         {
-            // 1. 获取用户输入的学号 (您的代码，无需改动)
             string studentNo = txtResetStudentNo.Text.Trim();
             if (string.IsNullOrEmpty(studentNo))
             {
@@ -91,11 +36,16 @@ namespace WindowsFormsApp1
                 return;
             }
 
+            // 如果安全问题框是可见的，先将其隐藏，避免界面混乱
+            if (gbSecurityQuestion.Visible)
+            {
+                gbSecurityQuestion.Visible = false;
+            }
+
             SQLHelper sh = new SQLHelper();
             try
             {
-                // 2. 根据学号查询用户的邮箱地址 (您的代码，无需改动)
-                string sqlGetEmail = string.Format("SELECT Email FROM tblTopStudents WHERE studentNo = '{0}'", studentNo);
+                string sqlGetEmail = $"SELECT Email FROM tblTopStudents WHERE studentNo = '{studentNo}'";
                 string userEmail = sh.RunSelectSQLToScalar(sqlGetEmail);
 
                 if (string.IsNullOrEmpty(userEmail))
@@ -104,63 +54,140 @@ namespace WindowsFormsApp1
                     return;
                 }
 
-                // 3. 生成一个新的8位随机临时密码 (您的代码，无需改动)
                 string tempPassword = System.IO.Path.GetRandomFileName().Replace(".", "").Substring(0, 8);
-
-                // 4. 将新生成的【临时密码】进行哈希加密 (您的代码，无需改动)
                 string hashedTempPassword = HashPassword(tempPassword);
+                string sqlUpdatePass = $"UPDATE tblTopStudents SET pwd = '{hashedTempPassword}' WHERE studentNo = '{studentNo}'";
+                sh.RunSQL(sqlUpdatePass);
 
-                // 5. 构建SQL语句，更新数据库中的密码 (您的代码，无需改动)
-                string sqlUpdatePass = string.Format("UPDATE tblTopStudents SET pwd = '{0}' WHERE studentNo = '{1}'", hashedTempPassword, studentNo);
-                int rowsAffected = sh.RunSQL(sqlUpdatePass);
-
-                if (rowsAffected == 0)
-                {
-                    MessageBox.Show("更新密码时发生未知错误，请稍后重试。", "数据库错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-
-                // 6. 调用邮件发送方法 (您的代码，无需改动)
                 SendEmail(userEmail, tempPassword);
 
-                // 7. 提示用户操作成功 (您的代码，无需改动)
                 MessageBox.Show("操作成功！\n一封包含临时密码的邮件已发送到您的邮箱，请查收并使用新密码登录。", "发送成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
                 this.Close();
             }
-            // === 以下是【增强版】的异常处理 ===
-            catch (SmtpException smtpEx) // 专门捕获邮件发送相关的异常
+            catch (Exception ex)
             {
-                // SmtpException 提供了更丰富的邮件错误信息
-                StringBuilder errorMessage = new StringBuilder();
-                errorMessage.AppendLine("邮件发送失败！请检查您的邮箱配置或网络。");
-                errorMessage.AppendLine("----------------------------------------");
-                errorMessage.AppendLine("错误类型: SMTP 错误");
-                errorMessage.AppendLine("错误代码: " + smtpEx.StatusCode); // 例如: 535, 451
-                errorMessage.AppendLine("详细信息: " + smtpEx.Message);
-
-                // 内部异常 InnerException 往往包含最根本的原因！
-                if (smtpEx.InnerException != null)
-                {
-                    errorMessage.AppendLine("根本原因: " + smtpEx.InnerException.Message);
-                }
-
-                MessageBox.Show(errorMessage.ToString(), "邮件发送错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            catch (System.Data.SqlClient.SqlException sqlEx) // 专门捕获数据库相关的异常
-            {
-                MessageBox.Show("操作失败，与数据库交互时发生错误。\n\n错误详情: " + sqlEx.Message, "数据库错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            catch (Exception ex) // 捕获其他所有未预料到的异常
-            {
-                MessageBox.Show("发生未知错误。\n\n错误详情: " + ex.Message, "系统错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("操作失败，请检查您的网络或邮箱配置。\n错误详情: " + ex.Message, "发送失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             finally
             {
-                // 无论成功还是失败，最后都要确保关闭数据库连接
                 sh.Close();
             }
         }
 
+        // 邮件发送方法 (代码保持不变)
+        private void SendEmail(string recipientEmail, string password)
+        {
+            string fromEmail = "1195773178@qq.com";
+            string fromPassword = "jidwtfljqebkicej"; // 这是邮箱的授权码，不是登录密码
+            string smtpHost = "smtp.qq.com";
+            int smtpPort = 587;
+
+            SmtpClient client = new SmtpClient(smtpHost, smtpPort);
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential(fromEmail, fromPassword);
+
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress(fromEmail, "学生管理系统");
+            mailMessage.To.Add(recipientEmail);
+            mailMessage.Subject = "【学生管理系统】 - 密码重置通知";
+            mailMessage.Body = $"您好：\n\n您正在使用密码重置功能。您的新临时密码是：\n\n【 {password} 】\n\n请使用此密码登录系统，并尽快在个人中心修改为您自己的常用密码。\n\n【学生管理系统】";
+            mailMessage.IsBodyHtml = false;
+            client.Send(mailMessage);
+        }
+
+        #endregion
+
+        #region =========== 2. 通过安全问题找回 ===========
+
+        // “使用安全问题找回”按钮的逻辑
+        private void btnShowSecurityQuestion_Click(object sender, EventArgs e)
+        {
+            if (txtResetStudentNo.Text.Trim().Equals(TargetStudentNo))
+            {
+                // 【硬编码】设置问题文本
+                lblQuestion1.Text = "问题1: 您最喜欢的颜色是什么?";
+                lblQuestion2.Text = "问题2: 您的小学叫什么名字?";
+
+                // 清空可能残留的答案输入
+                txtAnswer1.Clear();
+                txtAnswer2.Clear();
+
+                // 显示安全问题验证框
+                gbSecurityQuestion.Visible = true;
+            }
+            else
+            {
+                MessageBox.Show("此用户不支持通过安全问题找回密码，请使用邮箱方式或联系管理员。", "操作受限", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                // 确保在不支持的情况下，安全问题框是隐藏的
+                gbSecurityQuestion.Visible = false;
+            }
+        }
+
+        // “验证答案”按钮的逻辑
+        private void btnVerifyAnswers_Click(object sender, EventArgs e)
+        {
+            string answer1 = txtAnswer1.Text.Trim();
+            string answer2 = txtAnswer2.Text.Trim();
+
+            // 【硬编码】直接进行答案的字符串匹配
+            if (answer1.Equals("蓝色") && answer2.Equals("东二小学"))
+            {
+                try
+                {
+                    string tempPassword = System.IO.Path.GetRandomFileName().Replace(".", "").Substring(0, 8);
+                    string hashedTempPassword = HashPassword(tempPassword);
+
+                    SQLHelper sh = new SQLHelper();
+                    try
+                    {
+                        string sqlUpdatePass = $"UPDATE tblTopStudents SET pwd = '{hashedTempPassword}' WHERE studentNo = '{TargetStudentNo}'";
+                        sh.RunSQL(sqlUpdatePass);
+
+                        MessageBox.Show(
+                            $"验证成功！您的密码已重置。\n\n您的新临时密码是： 【 {tempPassword} 】\n\n请使用此密码登录。",
+                            "密码已重置",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+
+                        this.Close(); // 完成后关闭窗口
+                    }
+                    finally
+                    {
+                        sh.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("重置密码时发生错误: " + ex.Message, "系统错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("安全问题答案错误，请重试！", "验证失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        #endregion
+
+        #region =========== 3. 公用加密方法 ===========
+
+        // 密码哈希方法 (代码保持不变)
+        private string HashPassword(string plainText)
+        {
+            if (string.IsNullOrEmpty(plainText)) return string.Empty;
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(plainText));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        #endregion
     }
 }
